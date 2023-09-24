52d303ac7ded92e2721c442dedb386ec007240d9
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-24 17:21:01 -0300

    feat: sum all hours

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,38 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  @names [
+    "daniele",
+    "mayk",
+    "giuliano",
+    "cleiton",
+    "jakeliny",
+    "joseph",
+    "diego",
+    "danilo",
+    "rafael",
+    "vinicius"
+  ]
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  def build, do: {:error, "Insira o nome de um arquivo"}
+
+  defp sum_values([name, hours, _day, _month, _year], %{"all_hours" => all_hours}) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    build_report(all_hours)
+  end
+
+  defp report_acc do
+    people = Enum.into(@names, %{}, &{&1, 0})
+
+    %{"all_hours" => people}
+  end
+
+  defp build_report(all_hours), do: %{"all_hours" => all_hours}
 end
