de7132d2b7d61c1f2fbe22b36a4beb42fb101eed
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 07:51:22 -0300

    feat: sum working hours per month grouped by person

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -14,6 +14,21 @@ defmodule GenReport do
     "vinicius"
   ]
 
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -22,17 +37,30 @@ defmodule GenReport do
 
   def build, do: {:error, "Insira o nome de um arquivo"}
 
-  defp sum_values([name, hours, _day, _month, _year], %{"all_hours" => all_hours}) do
+  defp sum_values([name, hours, _day, month, _year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month
+       }) do
     all_hours = Map.put(all_hours, name, all_hours[name] + hours)
 
-    build_report(all_hours)
+    working_hours_per_month =
+      Map.put(hours_per_month[name], month, hours_per_month[name][month] + hours)
+
+    hours_per_month = Map.put(hours_per_month, name, working_hours_per_month)
+
+    build_report(all_hours, hours_per_month)
   end
 
   defp report_acc do
     people = Enum.into(@names, %{}, &{&1, 0})
+    months = Enum.into(@months, %{}, &{&1, 0})
 
-    %{"all_hours" => people}
+    hours_per_month = Enum.into(@names, %{}, &{&1, months})
+
+    build_report(people, hours_per_month)
   end
 
-  defp build_report(all_hours), do: %{"all_hours" => all_hours}
+  defp build_report(all_hours, hours_per_month) do
+    %{"all_hours" => all_hours, "hours_per_month" => hours_per_month}
+  end
 end
