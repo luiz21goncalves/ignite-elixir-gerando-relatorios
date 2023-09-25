5095eec5a663b2230a4328d1d8f7684eb1abfcf5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-25 19:38:10 -0300

    feat: build parallel reports

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -39,6 +39,47 @@ defmodule GenReport do
 
   def build, do: {:error, "Insira o nome de um arquivo"}
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Insira uma list de arquivos"}
+  end
+
+  def build_from_many(filenames) do
+    result =
+      filenames
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => hours_per_month1,
+           "hours_per_year" => hours_per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => hours_per_month2,
+           "hours_per_year" => hours_per_year2
+         }
+       ) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+
+    hours_per_month = merge_maps(hours_per_month1, hours_per_month2)
+
+    hours_per_year = merge_maps(hours_per_year1, hours_per_year2)
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn
+      _key, value1, value2 when is_map(value1) and is_map(value2) -> merge_maps(value1, value2)
+      _key, value1, value2 -> value1 + value2
+    end)
+  end
+
   defp sum_values([name, hours, _day, month, year], %{
          "all_hours" => all_hours,
          "hours_per_month" => hours_per_month,
